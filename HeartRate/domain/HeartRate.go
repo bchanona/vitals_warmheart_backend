de044eaf15a2685c63d1128e23296a9ef1d9a9b2
bchanona <bh274266@gmail.com>
2025-06-14 21:02:16 -0600

    feat(api): agregar DTO para el módulo HeartRate (models)

---
+++ HeartRate/domain/HeartRate.go
@@ -0,0 +1,32 @@
+package domain
+
+//DTO for heart rate data
+
+//model to save heart rate data
+
+type SaveHeartRateModel struct {
+	User_id     int     `json:"user_id"`
+	Measurement float64 `json:"measurement"`
+	Device_id   int     `json:"device_id"`
+}
+
+//model to get heart rate data
+type GetHeartRateModel struct {
+	Heart_rate_id int     `json:"heart_rate_id"`
+	User_id       int     `json:"user_id"`
+	Measurement   float64 `json:"measurement"`
+	Recorded_at   string  `json:"recorded_at"`
+	Device_id     int     `json:"device_id"`
+}
+
+//model to get heart rate data by user
+type GetHeartRateByUserModel struct {
+	Heart_rate_id int     `json:"heart_rate_id"`
+	User_id       int     `json:"user_id"`
+	Measurement   float64 `json:"measurement"`
+	Recorded_at   string  `json:"recorded_at"`
+	Name_user     string  `json:"name_user"`
+	Email_user    string  `json:"email_user"`
+	Premium_user  bool    `json:"premium_user"`
+	Device_id     int     `json:"device_id"`
+}
