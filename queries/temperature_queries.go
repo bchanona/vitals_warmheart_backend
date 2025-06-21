293fd4401b47fc15e192ae5345d18e62c476a72e
bchanona <bh274266@gmail.com>
2025-06-21 14:49:00 -0600

    add library

--- queries/temperature_queries.go
+++ queries/temperature_queries.go
@@ -6,3 +6,7 @@ INSERT INTO TEMPERATURES (user_id, measurement, device_id) VALUES (?,?,?)
 const GetAllTemperatures = `
 SELECT * FROM TEMPERATURES
 `
+const GetByDate = `
+SELECT measuremnt,date,time FROM TEMPERATURES
+WHERE user_id = ? AND date = ?
+`
