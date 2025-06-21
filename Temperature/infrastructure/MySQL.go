96c7c7213d226fe5d484c7c24a7a08968a219ca8
bchanona <bh274266@gmail.com>
2025-06-21 00:59:38 -0600

    feat(api): implement query

--- Temperature/infrastructure/MySQL.go
+++ Temperature/infrastructure/MySQL.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/bchanona/vitals_warmheart_backend/Temperature/domain"
 	"github.com/bchanona/vitals_warmheart_backend/queries"
@@ -18,4 +19,37 @@ func NewMySQL(db *sql.DB) *MySQL {
 func (sql *MySQL) Save(temperature domain.SaveTemperatureModel) error {
 	_, err := sql.db.Exec(queries.SaveTemperatureQuery, temperature.User_id, temperature.Measurement, temperature.Device_id)
 	return err
+}
+func (sql *MySQL) GetAll() ([]domain.GetTemperatureModel, error) {
+	var temperatures []domain.GetTemperatureModel
+
+	rows, err := sql.db.Query(queries.GetAllTemperatures)
+
+	if err != nil {
+		return nil, fmt.Errorf("error al ejecutar la consulta: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var temp domain.GetTemperatureModel
+
+		err := rows.Scan(
+			&temp.Temperature_id,
+			&temp.User_id,
+			&temp.Measurement,
+			&temp.Date,
+			&temp.Time,
+			&temp.Device_id,	
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error al escanear fila: %v", err)
+		}
+		temperatures = append(temperatures, temp)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("error al recorrer filas: %v", err)
+	}
+
+	return temperatures, nil
+
 }
