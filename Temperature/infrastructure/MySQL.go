ee1ce7c57607f40b94a3a3cf1d9ab6835f88aba7
bchanona <bh274266@gmail.com>
2025-06-21 14:47:49 -0600

    feat(api): implement query

--- Temperature/infrastructure/MySQL.go
+++ Temperature/infrastructure/MySQL.go
@@ -20,6 +20,7 @@ func (sql *MySQL) Save(temperature domain.SaveTemperatureModel) error {
 	_, err := sql.db.Exec(queries.SaveTemperatureQuery, temperature.User_id, temperature.Measurement, temperature.Device_id)
 	return err
 }
+
 func (sql *MySQL) GetAll() ([]domain.GetTemperatureModel, error) {
 	var temperatures []domain.GetTemperatureModel
 
@@ -39,7 +40,7 @@ func (sql *MySQL) GetAll() ([]domain.GetTemperatureModel, error) {
 			&temp.Measurement,
 			&temp.Date,
 			&temp.Time,
-			&temp.Device_id,	
+			&temp.Device_id,
 		)
 		if err != nil {
 			return nil, fmt.Errorf("error al escanear fila: %v", err)
@@ -51,5 +52,34 @@ func (sql *MySQL) GetAll() ([]domain.GetTemperatureModel, error) {
 	}
 
 	return temperatures, nil
+
+}
+
+func (sql *MySQL) GetByDate(user_id int, date string) ([]domain.GetTemperatureModel, error) {
+	var temperaturesByDate []domain.GetTemperatureModel
+
+	rows, err := sql.db.Query(queries.GetByDate, user_id, date)
+
+	if err != nil {
+		return nil, fmt.Errorf("error al ejecutar la consulta: %v", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var temperatureByD domain.GetTemperatureModel
+
+		err := rows.Scan(
+			&temperatureByD.Measurement,
+			&temperatureByD.Date,
+			&temperatureByD.Time,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error al escanear fila: %v", err)
+		}
+		temperaturesByDate = append(temperaturesByDate, temperatureByD)
+
+	}
+	return temperaturesByDate, nil
 
 }
