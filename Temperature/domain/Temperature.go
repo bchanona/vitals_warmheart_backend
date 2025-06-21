fc057ef577fa2f47a0db09d7a6c0ecd78df70b2b
bchanona <bh274266@gmail.com>
2025-06-21 00:55:59 -0600

    refactor(api): Fix DTO to get logs

--- Temperature/domain/Temperature.go
+++ Temperature/domain/Temperature.go
@@ -12,15 +12,17 @@ type GetTemperatureModel struct {
 	Temperature_id int     `json:"temperature_id"`
 	User_id        int     `json:"user_id"`
 	Measurement    float64 `json:"measurement"`
+	Date           string  `json:"date"`
+	Time           string  `json:"time"`
 	Device_id      int     `json:"device_id"`
-	Recorded_at    string  `json:"recorded_at"`
 }
 
 type GetTemperatureByUserModel struct {
 	Temperature_id int     `json:"temperature_id"`
 	User_id        int     `json:"user_id"`
 	Measurement    float64 `json:"measurement"`
-	Recorded_at    string  `json:"recorded_at"`
+	Date           string  `json:"date"`
+	Time           string  `json:"time"`
 	Name_user      string  `json:"name_user"`
 	Surname_user   string  `json:"surname_user"`
 	Email_user     string  `json:"email_user"`
