89383098d510066c1491ba3c0f8b886124e3204b
bchanona <bh274266@gmail.com>
2025-06-26 23:45:16 -0600

    fix(api): correcting model

--- HeartRate/domain/HeartRate.go
+++ HeartRate/domain/HeartRate.go
@@ -12,22 +12,17 @@ type SaveHeartRateModel struct {
 
 //model to get heart rate data
 type GetHeartRateModel struct {
-	Heart_rate_id int     `json:"heart_rate_id"`
-	User_id       int     `json:"user_id"`
-	Measurement   float64 `json:"measurement"`
-	Recorded_at   string  `json:"recorded_at"`
-	Device_id     int     `json:"device_id"`
+	Measurement    float64 `json:"measurement"`
+	Date           string  `json:"date"`
+	Time           string  `json:"time"`
 }
 
 //model to get heart rate data by user
 type GetHeartRateByUserModel struct {
-	Heart_rate_id int     `json:"heart_rate_id"`
-	User_id       int     `json:"user_id"`
-	Measurement   float64 `json:"measurement"`
-	Recorded_at   string  `json:"recorded_at"`
-	Name_user     string  `json:"name_user"`
-	Surname_user  string  `json:"surname_user"`
-	Email_user    string  `json:"email_user"`
-	Premium_user  bool    `json:"premium_user"`
-	Device_id     int     `json:"device_id"`
+	Measurement    float64 `json:"measurement"`
+	Date           string  `json:"date"`
+	Time           string  `json:"time"`
+	Name_user      string  `json:"name_user"`
+	Surname_user   string  `json:"surname_user"`
+	Email_user     string  `json:"email_user"`
 }
