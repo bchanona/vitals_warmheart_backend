d1cd327cdb1d6a3171866bec9b17e6cc9581f22e
bchanona <bh274266@gmail.com>
2025-07-02 00:56:44 -0600

    feat(api):communication with the database

---
+++ Oxygen/infrastructure/MySQL.go
@@ -0,0 +1,65 @@
+package infrastructure
+
+import (
+	"database/sql"
+
+	"github.com/bchanona/vitals_warmheart_backend/Oxygen/domain"
+	"github.com/bchanona/vitals_warmheart_backend/queries"
+)
+
+type MySQL struct {
+	db *sql.DB
+}
+
+func NewMySQL(db *sql.DB) *MySQL {
+	return &MySQL{db: db}
+}
+
+func (sql *MySQL) Save(oxygen domain.SaveOxygenModel) error {
+	_, err := sql.db.Exec(queries.SaveOxygen, oxygen.User_id, oxygen.Measurement, oxygen.Device_id)
+	return err
+}
+func (sql *MySQL) GetAll() ([]domain.GetOxygenModel, error) {
+	rows, err := sql.db.Query(queries.GetAllOxygen)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var oxygenData []domain.GetOxygenModel
+	for rows.Next() {
+		var oxygen domain.GetOxygenModel
+		if err := rows.Scan(&oxygen.Measurement, &oxygen.Date, &oxygen.Time); err != nil {
+			return nil, err
+		}
+		oxygenData = append(oxygenData, oxygen)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return oxygenData, nil
+}
+func (sql *MySQL) GetOxygenByDate(userId int, date string) ([]domain.GetOxygenModel, error) {
+	rows, err := sql.db.Query(queries.GetOxygenByDate, userId, date)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var oxygenByDate []domain.GetOxygenModel
+	for rows.Next() {
+		var oxygenBD domain.GetOxygenModel
+		if err := rows.Scan(&oxygenBD.Measurement, &oxygenBD.Date, &oxygenBD.Time); err != nil {
+			return nil, err
+		}
+		oxygenByDate = append(oxygenByDate, oxygenBD)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return oxygenByDate, nil
+}
