03a7630cb30578c6f7dfbda24895127250905018
bchanona <bh274266@gmail.com>
2025-07-05 20:30:24 -0600

    Add cors and protected routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	oxygenDependencies "github.com/bchanona/vitals_warmheart_backend/Oxygen/infrastructure/dependencies"
 	oxygenRoutes "github.com/bchanona/vitals_warmheart_backend/Oxygen/infrastructure/routes"
+	"github.com/bchanona/vitals_warmheart_backend/helpers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	oxygenDependencies.Init()
 
 	r:= gin.Default()
+	helpers.InitCORS(r)
 	oxygenRoutes.Routes(r)
 
 	r.Run(":8080")
