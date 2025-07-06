9bbaa204918008175b99ebd79f188603d01307b4
bchanona <bh274266@gmail.com>
2025-07-05 20:23:31 -0600

    Add cors and protected routes

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	temperatureDependencies "github.com/bchanona/vitals_warmheart_backend/Temperature/infrastructure/dependencies"
 	temperatureRoutes "github.com/bchanona/vitals_warmheart_backend/Temperature/infrastructure/routes"
+	"github.com/bchanona/vitals_warmheart_backend/helpers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Initialize the database connection
 	temperatureDependencies.Init()
+	
 
 	// Create a new Gin router
 	r := gin.Default()
+	helpers.InitCORS(r)
 	temperatureRoutes.Routes(r)
 
 	r.Run(":8080") // Start the server on port 8080
---
+++ middlewares/auth_user.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var jwtKey = []byte("escribir_la_clave_secreta_aqui") // Cambia esto por tu clave secreta
+
+type Claims struct {
+	User_id int `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString := ctx.GetHeader("Authorization")
+
+		// Verificar si el token existe y tiene el formato correcto
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token no proporcionado"})
+			ctx.Abort()
+			return
+		}
+
+		// Extraer el token quitando "Bearer "
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		// Parsear el token
+		claims := &Claims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		// Validar si el token es válido
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.Abort()
+			return
+		}
+
+		// Guardar el ID del usuario en el contexto para usarlo en controladores
+		ctx.Set("user_id", claims.User_id)
+		ctx.Next()
+	}
+}
