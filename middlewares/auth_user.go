7e5f83f1121e617c84dd67b5ba972efb5cde2386
bchanona <bh274266@gmail.com>
2025-07-10 02:41:56 -0600

    feat(api): add middlewares

--- middlewares/auth_user.go
+++ middlewares/auth_user.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-var jwtKey = []byte("escribir_la_clave_secreta_aqui") // Cambia esto por tu clave secreta
+var jwtKey = []byte("clave02security") // Cambia esto por tu clave secreta
 
 type Claims struct {
 	User_id int `json:"user_id"`
