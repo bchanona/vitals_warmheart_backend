35a3443b30b8f1ea61e959af83ea265ecb8a7bfc
bchanona <bh274266@gmail.com>
2025-07-18 00:38:23 -0600

    feat(api): updating main

--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	heartRateDependencies "github.com/bchanona/vitals_warmheart_backend/HeartRate/infrastructure/dependencies"
+	customRhythmDependencies "github.com/bchanona/vitals_warmheart_backend/customRhythm/infrastructure/dependencies"
 	"github.com/bchanona/vitals_warmheart_backend/helpers"
 	"github.com/gin-gonic/gin"
 	heartRatesRoutes "github.com/bchanona/vitals_warmheart_backend/HeartRate/infrastructure/routes"
+	customRhythmRoutes "github.com/bchanona/vitals_warmheart_backend/customRhythm/infrastructure/routes" 
 )
 
 func main() {
 	
 	heartRateDependencies.Init()
+	customRhythmDependencies.Init()
 
 	r:= gin.Default()
 	helpers.InitCORS(r)
 
 	heartRatesRoutes.Routes(r)
+	customRhythmRoutes.Routes(r)
 
 	r.Run(":8080") 
 
