6688fa7b4d98e33a894c0d411aeeb94d43c193d2
bchanona <bh274266@gmail.com>
2025-07-21 16:06:18 -0600

    fix(api): correct main

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ import (
 
 func main() {
 	oxygenDependencies.Init()
-  temperatureDependencies.Init()
-  heartRateDependencies.Init()
-  customRhythmDependencies.Init()
+  	temperatureDependencies.Init()
+  	heartRateDependencies.Init()
+  	customRhythmDependencies.Init()
 	
 
 
@@ -29,7 +29,8 @@ func main() {
 
 	customRhythmRoutes.Routes(r)
 	temperatureRoutes.Routes(r)
-  oxygenRoutes.Routes(r)
+  	oxygenRoutes.Routes(r)
+	heartRatesRoutes.Routes(r)
 
 	r.Run(":8080") // Start the server on port 8080
 
